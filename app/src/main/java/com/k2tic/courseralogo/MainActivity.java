2a0bb376981e2313784a13771c2208dafd283df3
lalbertogith <lmoranchel@gmail.com>
2017-02-14 21:27:19 +0100

    Initial commit

---
+++ app/src/main/java/com/k2tic/courseralogo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.k2tic.courseralogo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
